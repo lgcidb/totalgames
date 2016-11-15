b74da6a7de43c353b7293dc9e8b17faf43d30095
dfabrega991 <dfabrega991@gmail.com>
2016-11-14 21:59:21 -0300

    prueba

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Total Games',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -23,8 +23,7 @@ return array(
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'totalgames',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'password'=>false,
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		
@@ -50,8 +49,16 @@ return array(
 		),
 		*/
 
-		// database settings are configured in database.php
-		'db'=>require(dirname(__FILE__).'/database.php'),
+		/*// database settings are configured in database.php
+		'db'=>require(dirname(__FILE__).'/database.php'),*/
+
+		'db'=>array(
+'connectionString' => 'mysql:host=localhost;dbname=totalgames',
+'emulatePrepare' => true,
+'username' => 'root',
+'password' => false,
+'charset' => 'utf8',
+),
 
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,9 +30,9 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label' => 'Juegos', 'url' => array('/juego/admin')),
+				array('label' => 'Operaciones', 'url' => array('/operacion/admin')),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
